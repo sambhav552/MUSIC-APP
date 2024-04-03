9917e6151b7f37122748e75d7e429dbf6d1832d8
Saksham <sambhavgupta295@gmail.com>
2024-04-03 08:39:58 +0530

    prototype

--- app.js
+++ app.js
@@ -147,10 +147,22 @@ masterPlay.addEventListener('click',()=>{
         masterPlay.classList.add('bi-play-fill');
     }
 })
+const makeAllplays=()=>{
+    Array.from(document.getElementsByClassName('playListPlay')).forEach((el)=>{
+        el.classList.add('bi-play-circle-fill');
+        el.classList.remove('bi-pause-circle-fill');
+    })
+}
 
+const makeAllBackground=()=>{
+    Array.from(document.getElementsByClassName('songItem')).forEach((el)=>{
+        el.style.background='rgba(105,105,105,.0)';
+    })
+}
 
 let index=0;
 let poster_master_play=document.getElementById('poster_master_play');
+let title=document.getElementById('title');
 Array.from(document.getElementsByClassName('playlistPlay')).forEach((e)=>{
     e.addEventListener('click',(el)=>{
        index=el.target.id;
@@ -160,12 +172,89 @@ Array.from(document.getElementsByClassName('playlistPlay')).forEach((e)=>{
         music.play()
         masterPlay.classList.remove('bi-play-fill');
         masterPlay.classList.add('bi-pause-fill');
+
+        let songTitles=songs.filter((els)=>{
+            return els.id==index;
+        });
+
+        songTitles.forEach(elss=>{
+            let {songName}=elss;
+            title.innerHTML=songName;
+        });
+
+        makeAllBackground();
+        Array.from(document.getElementsByClassName('songItem'))[index-1].style.background='rgba(105,105,105,.1)';
+
+        makeAllplays();
+
+        e.target.classList.remove('bi-play-circle-fill')
+        e.target.classList.add('bi-pause-circle-fill')
+        wave.classList.add('active1');
     })
 })
 
+let currentStart=document.getElementById('currentStart');
+let currentEnd=document.getElementById('currentEnd');
+let seek=document.getElementById('seek');
+let bar2=document.getElementById('bar2');
+let dot=document.getElementsByClassName('dot')[0];
+
+music.addEventListener('timeupdate',()=>{
+    let music_curr=music.currentTime;
+    let music_dur=music.duration;
 
 
+    let min1=Math.floor(music_dur/60)
+    let sec1=Math.floor(music_dur%60)
 
+    if(sec1<10){
+        sec1=`0${sec1}`
+    }
+    currentEnd.innerText=`${min1}:${sec1}`
+
+    let min2=Math.floor(music_curr/60);
+    let sec2=Math.floor(music_curr%60)
+    if(sec2<10){
+        sec2=`0${sec2}`
+    }
+    currentStart.innerText=`${min2}:${sec2}`
+
+    let progressBar=parseInt((music_curr/music_dur)*100);
+    seek.value=progressBar;
+
+    let seekBar=seek.value;
+    bar2.style.width=`${seekBar}%`
+    dot.style.left=`${seekBar}%`
+
+
+})
+
+seek.addEventListener('change',()=>{
+    music.currentTime=seek.value*music.duration/100;
+})
+
+let vol_icon=document.getElementById('vol-icon');
+let vol=document.getElementById('vol');
+let vol_bar=document.getElementsByClassName('vol-bar')[0];
+let vol_dot=document.getElementById('vol-dot');
+
+vol.addEventListener('change',()=>{
+    if(vol.value==0){
+        vol_icon.classList.remove('bi-volume-up-fill')
+        vol_icon.classList.remove('bi-volume-down-fill')
+        vol_icon.classList.add('bi-volume-off-fill')
+    }
+    if(vol.value>0){
+        vol_icon.classList.remove('bi-volume-up-fill')
+        vol_icon.classList.add('bi-volume-down-fill')
+        vol_icon.classList.remove('bi-volume-off-fill')
+    }
+    if(vol.value>50){
+        vol_icon.classList.add('bi-volume-up-fill')
+        vol_icon.classList.remove('bi-volume-down-fill')
+        vol_icon.classList.remove('bi-volume-off-fill')
+    }
+})
 
 
 
